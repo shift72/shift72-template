cdfcbf284882c4815abd3258123777f6a999a66b
stajs <shannon@shift72.com>
2021-05-17 14:36:33 +1200

    AB#2850 Use NPM

--- tools/build-output.js
+++ tools/build-output.js
@@ -1,32 +1,32 @@
 const fse = require('fs-extra');
 const path = require('path');
 const del = require('del');
-
-const args = require('yargs').argv
+const args = require('yargs').argv;
+const chalk = require('chalk');
 
 const copyFilter = (src, dest) => {
   let basename = path.basename(src);
 
   // Don't copy .git files
-  if(basename.indexOf('.git') == 0) {
+  if (basename.indexOf('.git') == 0) {
     return false;
   }
 
   // Don't copy DS_Store files
-  if(basename.indexOf('.DS_Store') == 0) {
+  if (basename.indexOf('.DS_Store') == 0) {
     return false;
   }
 
   // Don't copy README files
-  if(basename.indexOf('README.md') == 0) {
+  if (basename.indexOf('README.md') == 0) {
     return false;
   }
 
   // Ignore these paths if this is a simple template/static image update
-  if(args.mode == 'kibble:watch:local') {
-  	if(basename.indexOf('_variables.scss') == 0) {
-  		return false
-  	}
+  if (args.mode == 'kibble:watch:local') {
+    if (basename.indexOf('_variables.scss') == 0) {
+      return false
+    }
   }
 
   return true;
@@ -36,15 +36,19 @@ const copyFilter = (src, dest) => {
 fse.ensureDirSync('./output');
 
 // Only run these updates on start up
-if(args.mode == 'init') {
-	// Empty out destination directory except for .kibble
-	paths = del.sync(['./output/**' ,'!./output/.kibble'], { dot: true });
+if (args.mode == 'init') {
+  // Empty out destination directory except for .kibble
+  paths = del.sync(['./output/**', '!./output/.kibble'], { dot: true });
 
-	// Delete .kibble except for .kibble/private.json if it exists
-	paths = del.sync(['./output/.kibble/**' ,'!./output/.kibble/private.json'], { dot: true });
+  // Delete .kibble except for .kibble/private.json if it exists
+  paths = del.sync(['./output/.kibble/**', '!./output/.kibble/private.json'], { dot: true });
 
-	// Copy core into output wholesale
-	fse.copySync('./core', './output', copyFilter);
+  // Copy core into output
+  const coreDir = './node_modules/@shift72/core-template/';
+  const coreVersion = fse.readJsonSync(coreDir + 'package.json').version;
+  console.log(chalk.green(`core-template@${coreVersion}`));
+  fse.copySync(coreDir + 'site', './output/site', copyFilter);
+  fse.copySync(coreDir + 'kibble.json', './output/kibble.json', copyFilter);
 }
 
 // And then copy local into output
